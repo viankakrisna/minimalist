7b59e4a74c11032e5129406ded358c0e04a25c50
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-01-29 05:14:49 +0700

    add comments

--- src/styled.js
+++ src/styled.js
@@ -1,6 +1,8 @@
 import { e } from './elements';
 
+//take the exp and props variable, and create a reducer function for that.
 function createRawCSSReducer(exp, props) {
+  //combine the array of css.raw to a string, and invoke any function with props
   return function rawCSSReducer(res, string, i) {
     if (typeof exp[i] === 'function') {
       res += string + exp[i](props);
@@ -11,25 +13,35 @@ function createRawCSSReducer(exp, props) {
   };
 }
 
+//styled-components wannabe! take a component 
 export default function styled(Component) {
+  //then create tagged template literal for that component
   return function createStyled(css, ...exp) {
+    //return a named function, easier to debug in the stack trace
     return function styledComponent(props) {
       const interpolatedCSS = css.raw.reduce(
         createRawCSSReducer(exp, props),
         ''
       );
+      
+      //use the component function name, else just use styled prefix
       const className = [
         Component.name || 'styled',
-        hashCode(interpolatedCSS)
+        hashCode(interpolatedCSS) //generates a unique identifier based on the css string
       ].join('_');
-      const styleAppended = document.getElementById(className);
-      const style = document.createElement('style');
-      style.innerHTML = interpolatedCSS.replace(/\&/g, '.' + className);
-      style.id = className;
-
+     
+      //only append the style in the head if it's not there
+      const styleAppended = document.getElementById(className);      
       if (!styleAppended) {
+        //create the style node
+        const style = document.createElement('style');
+        style.innerHTML = interpolatedCSS.replace(/\&/g, '.' + className);
+        style.id = className;
+
+        //append it!
         document.head.appendChild(style);
       }
+      
       return e(Component, { ...props, className });
     };
   };
