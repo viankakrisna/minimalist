ac4b253863f276b52db8c4ffc5d019566f0bd87f
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-02-06 01:55:07 +0700

    tweaks

--- webpack.server.js
+++ webpack.server.js
@@ -2,10 +2,14 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config.js');
 var spawn = require('child_process').spawn;
+var PORT = process.env.PORT || 8080;
+var HOST = process.env.HOST || 'localhost';
+var PROTOCOL = process.env.PROTOCOL || 'http';
+var ADDRESS = `${PROTOCOL}://${HOST}:${PORT}`;
 
 config.devtool = 'source-map';
 config.debug = true;
-config.entry.unshift('webpack-dev-server/client?http://localhost:8080', 'webpack/hot/dev-server');
+config.entry.unshift(`webpack-dev-server/client?${ADDRESS}`, 'webpack/hot/dev-server');
 config.plugins = config.plugins.concat(new webpack.HotModuleReplacementPlugin());
 
 // Start a webpack-dev-server
@@ -15,12 +19,13 @@ new WebpackDevServer(webpack(config), {
   },
   historyApiFallback: true,
   hot: true
-}).listen(8080, 'localhost', console.error);
+}).listen(PORT, HOST, console.error);
+
 switch (require('os').type()) {
   case 'Linux':
-    spawn('xdg-open', ['http://localhost:8080']);
+    spawn('xdg-open', [ADDRESS]);
     break;
   default:
-    spawn('open', ['http://localhost:8080']);
+    spawn('open', [ADDRESS]);
     break;
 }
